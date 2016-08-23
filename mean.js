c8f9758930ce6ba5b84ca8f92a4dd306c7f50e24
Antonio Montalvo <montalvocode@yahoo.com>
2016-08-23 08:53:47 -0700

    calculate the average

---
+++ mean.js
@@ -0,0 +1,7 @@
+allScores = [1, 2, 3, 4, 5];
+var avg = 0;
+for (var i = 0; i < allScores.length; i++){
+    avg += allScores[i]/allScores.length;
+}
+console.log (avg);
+console.log(allScores.length);
